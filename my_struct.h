837525b81cb41ea6914a33170478165bf210a129
AntoninJuquel <antonin.juquel@outlook.fr>
2022-01-28 10:46:08 +0100

    Added skeleton of my_struct

--- my_struct.h
+++ my_struct.h
@@ -1,6 +1,17 @@
 #ifndef __MY_STRUCT_H__
 #define __MY_STRUCT_H__
 
+typedef struct struct_my_struct
+{
+    
+} s_my_struct;
 
+typedef s_my_struct *p_s_my_struct;
+
+p_s_my_struct my_struct_alloc();
+void my_struct_free(p_s_my_struct p_vector);
+void my_struct_randoms_init(p_s_my_struct p_vector);
+void my_struct_copy(p_s_my_struct p_dest, p_s_my_struct p_src);
+int my_struct_cmp(p_s_my_struct p_vector_a, p_s_my_struct p_vector_b);
 
 #endif
